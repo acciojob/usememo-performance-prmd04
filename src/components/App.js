ee25efbc128384721d37a6cb8fa4ca39a89c3367
pramod ade <prmdextracthere@gmail.com>
2025-10-08 13:42:07 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import "./../styles/App.css";
+import tasks from "./data";
+import { useMemo } from "react";
 const App = () => {
+  const [currentTab, setCurrentTab] = useState('All');
+
+  const filteredData = useMemo(() => {
+    return currentTab === 'Active' ? tasks.filter(task => task.completed === false) : currentTab === 'Completed' ? tasks.filter(task => task.completed === true) : tasks;
+  }, [currentTab])
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>
+        <button onClick={() => setCurrentTab('All')}>All</button>
+        <button onClick={() => setCurrentTab('Active')}>Active</button>
+        <button onClick={() => setCurrentTab('Completed')}>Completed</button>
+      </div>
+      <ul>
+        {filteredData.map((task, index) => (
+          <li
+            key={index}
+            style={task.completed ? { textDecoration: "line-through" } : {}}
+          >
+            {task.task}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
